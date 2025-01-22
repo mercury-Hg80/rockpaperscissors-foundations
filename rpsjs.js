18b80ef2926e660c2ae57faba41897b1ce8548b4
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2025-01-22 10:41:26 -0500

    Feat add function getComputerChoice

---
+++ rpsjs.js
@@ -0,0 +1,23 @@
+/* Call function to make choice of 3 parameters */
+/* Function must determine relationship of parameter to number */
+/* Through random number generator (aka Math.random()) will invoke a parameter */
+/* Math.random() method will return a number >= to 0 or < 1 */
+
+let text;
+
+
+<script> 
+    
+    function getComputerChoice("rock", "paper", "scissors") {
+        if (Math.random() >= 0 && < 1) {
+            text = "rock"; 
+
+        } else if (Math.random() <= 0 && < 1) {
+            text = "paper";
+        } else if (Math.random () = 0 && < 1) {
+            text = "scissors";
+        }
+
+    
+    }
+</script>
