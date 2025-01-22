4a1f08228df686303720902c77deeeb697af0cb4
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2025-01-22 11:02:00 -0500

    Fix getComputerChoice syntax

--- rpsjs.js
+++ rpsjs.js
@@ -3,21 +3,31 @@
 /* Through random number generator (aka Math.random()) will invoke a parameter */
 /* Math.random() method will return a number >= to 0 or < 1 */
 
-let text;
 
-
-<script> 
+function getComputerChoice(){
     
-    function getComputerChoice("rock", "paper", "scissors") {
-        if (Math.random() >= 0 && < 1) {
-            text = "rock"; 
+    let randomNumber = Math.random();
 
-        } else if (Math.random() <= 0 && < 1) {
-            text = "paper";
-        } else if (Math.random () = 0 && < 1) {
-            text = "scissors";
-        }
+    if (randomNumber < 0.30) {
+        return "rock" 
+    }
+    
+    else if (randomNumber < 0.60) {
+        return "paper"
+    }
+
+    else {
+        return "scissors"
+    }
+    
 
     
     }
-</script>
+
+
+
+
+
+
+
+
