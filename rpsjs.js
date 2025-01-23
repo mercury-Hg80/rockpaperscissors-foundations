5b4eedae8d77bf71be4c04bd8f96006108d4651b
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2025-01-23 14:23:41 -0500

    Fix: enable playRound function to count scores per choice

--- rpsjs.js
+++ rpsjs.js
@@ -44,48 +44,51 @@ let computerScore = 0;
 /* Score will go up by increments of 1 depending on winner */
 /* humanChoice will be case insensitive */
 /* Set conditions for winner and loser return responses */
+/* WHEN YOU WORK ON THIS NEXT, MOVE THE SCORES INTO THE FUNCTIONS */
 
 function playRound(humanChoice, computerChoice) {
 
     humanChoice.toLowerCase()
     
     if (humanChoice === 'rock' && computerChoice === 'paper') {
+        computerScore++;
         return "You lose! Paper beats rock." 
     }
-    computerScore++;
+    
 
     else if (humanChoice === 'rock' && computerChoice === 'rock') {
         return "Same response! No one wins a point."
     }
 
     else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        humanScore++;
         return "You win! Rock beats scissors."
     }
-    humanScore++;
 
     else if (humanChoice === 'paper' && computerChoice === 'paper') {
         return "Same response! No one wins a point."
     }
 
     else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        humanScore++;
         return "You win! Paper beats rock."
     }
-    humanScore++;
 
     else if (humanChoice === 'paper' && computerChoice === 'scissors'){
+        computerScore++;
         return "You lose! Scissors beats paper."
     }
-    computerScore++;
 
     else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        humanScore++;
         return "You win! Scissors beats paper"
     }
-    humanScore++;
+    
 
     else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        computerScore++
         return "You lose! Rock beats scissors."
     }
-    computerScore++;
 
     else if (humanChoice === 'scissors' && computerChoice === 'scissors') {
         return "Same response! No one wins a point."
@@ -100,7 +103,10 @@ function playRound(humanChoice, computerChoice) {
 
 }
 
+function playGame() {
+    function playRound()
 
+}
 
 
     
